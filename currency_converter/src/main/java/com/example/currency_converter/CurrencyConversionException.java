7b917dd573285cd8a924e0154374a5311792ffaa
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2025-02-11 14:13:17 +0530

    Create CurrencyConversionException.java

---
+++ currency_converter/src/main/java/com/example/currency_converter/CurrencyConversionException.java
@@ -0,0 +1,8 @@
+package com.example.currency_converter;
+
+
+public class CurrencyConversionException extends RuntimeException {
+    public CurrencyConversionException(String message) {
+        super(message);
+    }
+}
