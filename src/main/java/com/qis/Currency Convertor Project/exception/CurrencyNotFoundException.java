f490da4cc44b52cfefec621d89e74108764c81d7
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2025-02-11 14:03:33 +0530

    Delete src/main directory

--- src/main/java/com/qis/Currency Convertor Project/exception/CurrencyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.qis.CurrencyConvertorProject.exception;
-
-public class CurrencyNotFoundException extends RuntimeException {
-    public CurrencyNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/qis/Currency Convertor Project/exception/ExternalApiException.java
+++
@@ -1,7 +0,0 @@
-package com.qis.CurrencyConvertorProject.exception;
-
-public class ExternalApiException extends RuntimeException {
-    public ExternalApiException(String message) {
-        super(message);
-    }
-}
